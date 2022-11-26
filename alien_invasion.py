bc360c7da3b268216f103aab359bde69782dc424
Fran Farina <farinajf@gmail.com>
2022-11-26 21:28:57 +0100

    Ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
-
 import pygame
+
+from settings import Settings
+from ship     import Ship
 
 class AlienInvasion:
 
@@ -8,24 +10,44 @@ class AlienInvasion:
 
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1600, 1200))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        self.bg_color = (180, 180, 180)
+        self.ship = Ship(self)
 
 
     def run_game(self):
-
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            #Redraw the screen
-            self.screen.fill(self.bg_color)
 
-            #Make the most recent screen visible
-            pygame.display.flip()
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+
+    def _update_screen(self):
+        #Redraw the screen
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        #Make the most recent screen visible
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """A class to store all settings for Alien Invasion."""
+
+    def __init__(self):
+
+        self.screen_width  = 1600
+        self.screen_height = 1200
+        #self.bg_color      = (180, 180, 180)
+        self.bg_color      = (230, 230, 230)
+
+        self.ship_speed = 1.5
