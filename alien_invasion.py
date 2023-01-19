5b65c081d504eb3392fbe0458855d54ad060f785
Fran Farina <farinajf@gmail.com>
2023-01-19 22:02:34 +0100

    Aliens are moving

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,6 +37,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
 
@@ -80,6 +81,16 @@ class AlienInvasion:
                 if b.rect.bottom <= 0:
                     self.bullets.remove(b)
 
+        self._check_bullet_alien_collisions()
+
+
+    def _check_bullet_alien_collisions(self):
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+            self.bullets.empty()
+            self._create_fleet()
+
 
     def _update_screen(self):
         #Redraw the screen
@@ -117,10 +128,32 @@ class AlienInvasion:
 
         alien_width, alien_height = alien.rect.size
 
-        alien.rect.x = alien_width  + 2 * alien_width * alien_number
+        alien.x      = alien_width  + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
         alien.rect.y = alien_height + 2 * alien.rect.height * row_number
 
         self.aliens.add(alien)
+
+
+    def _check_fleet_edges(self):
+
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+
+    def _change_fleet_direction(self):
+
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+
+    def _update_aliens(self):
+
+        self._check_fleet_edges()
+        self.aliens.update()
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -3,6 +3,10 @@ class Settings:
 
     def __init__(self):
 
+        self.alien_speed      = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction  = 1 # 1 -> right | -1 -> left
+
         self.screen_width  = 1600
         self.screen_height = 1200
         #self.bg_color      = (180, 180, 180)
@@ -11,7 +15,7 @@ class Settings:
         self.ship_speed = 1.5
 
         #Bullets
-        self.bullet_speed    = 1.0
+        self.bullet_speed    = 1.5
         self.bullet_width    = 3
         self.bullet_height   = 15
         self.bullet_color    = (60, 60, 60)
